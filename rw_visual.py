2f2674889c48ac9a5063c44dac9024de531f7a53
vin <1418545473@qq.com>
2017-09-04 16:57:15 +0800

    add point_numbers

--- rw_visual.py
+++ rw_visual.py
@@ -7,8 +7,10 @@ while True:
     #创建一个RandoWalk实例，并将其包含的点都绘制出来
     rw = RandoWalk()
     rw.fill_walk()
+    point_numbers = list(range(rw.num_points))
     #plt.axis([0,5000,0,5000])
-    plt.scatter(rw.x_values,rw.y_values,s=15,c=rw.y_values,cmap=plt.cm.Set1,edgecolors="none")
+    #plt.scatter(rw.x_values,rw.y_values,s=15,c=rw.y_values,cmap=plt.cm.Set1,edgecolors="none")
+    plt.scatter(rw.x_values,rw.y_values,s=15,c=point_numbers,cmap=plt.cm.Reds,edgecolors="none")
     plt.show()
 
     keep_running = input("Make another walk?(y/n):")
