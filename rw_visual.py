c05ccae30c37faf0ff3e86349668a3ce459cd3fe
vin <1418545473@qq.com>
2017-09-06 10:10:51 +0800

    give a test

--- rw_visual.py
+++ rw_visual.py
@@ -13,12 +13,12 @@ while True:
     #绘制点并将图形显示出来
     point_numbers = list(range(rw.num_points))
     #plt.axis([0,5000,0,5000])
-    #plt.scatter(rw.x_values,rw.y_values,s=15,c=rw.y_values,cmap=plt.cm.Set1,edgecolors="none")
+    plt.scatter(rw.x_values,rw.y_values,s=15,c=rw.y_values,cmap=plt.cm.Set1,edgecolors="none")
     #plt.scatter(rw.x_values,rw.y_values,s=1,c=point_numbers,cmap=plt.cm.Reds,edgecolors="none")
-    plt.plot(rw.x_values,rw.y_values,linewidth=1)
+    #plt.plot(rw.x_values,rw.y_values,linewidth=1)
     #突出起点和终点
-    #plt.scatter(0,0,c='green',edgecolors='none',s=100)
-    #plt.scatter(rw.x_values[-1],rw.y_values[-1],c='red',edgecolors='none',s=100)
+    plt.scatter(0,0,c='green',edgecolors='none',s=100)
+    plt.scatter(rw.x_values[-1],rw.y_values[-1],c='red',edgecolors='none',s=100)
     #隐藏坐标轴
     plt.axes().get_xaxis().set_visible(False)
     plt.axes().get_yaxis().set_visible(False)
